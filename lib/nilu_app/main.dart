1ee4dab34349c18698e1ea9bf55600f5a62d30de
Salma Maarouf <68617759+SalmaW@users.noreply.github.com>
2024-06-01 05:11:29 +0300

    task 12 - main()

---
+++ lib/nilu_app/main.dart
@@ -0,0 +1,72 @@
+import '../nilu_app/helpers/sql_helper.dart';
+import 'package:get_it/get_it.dart';
+
+import '../nilu_app/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var sqlHelper = SqlHelper();
+  await sqlHelper.init();
+  if (sqlHelper.db != null) {
+    GetIt.I.registerSingleton(sqlHelper);
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Easy Pos',
+      theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: seedColor), //used when have dominated palette
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff0157db),
+          foregroundColor: Colors.white,
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          errorColor: Colors.red,
+          cardColor: Colors.blue.shade100,
+          backgroundColor: Colors.white,
+          primarySwatch: getMaterialColor(
+              const Color(0xff0157db)), // convert color to materialColor
+        ),
+        useMaterial3: true,
+      ),
+      // routes: {//navigation by name
+      //   '/': (context) => Home(),
+      //   '/vpizza': (context) => VPizzaPage(),
+      //   '/cpizza': (context) => CPizzaPage(),
+      //   '/fries': (context) => FriesPage(),
+      // },
+      home: const HomePage(),
+    );
+  }
+
+  MaterialColor getMaterialColor(Color color) {
+    final int red = color.red;
+    final int green = color.green;
+    final int blue = color.blue;
+
+    final Map<int, Color> shades = {
+      50: Color.fromRGBO(red, green, blue, .1),
+      100: Color.fromRGBO(red, green, blue, .2),
+      200: Color.fromRGBO(red, green, blue, .3),
+      300: Color.fromRGBO(red, green, blue, .4),
+      400: Color.fromRGBO(red, green, blue, .5),
+      500: Color.fromRGBO(red, green, blue, .6),
+      600: Color.fromRGBO(red, green, blue, .7),
+      700: Color.fromRGBO(red, green, blue, .8),
+      800: Color.fromRGBO(red, green, blue, .9),
+      900: Color.fromRGBO(red, green, blue, 1),
+    };
+
+    return MaterialColor(color.value, shades);
+  }
+}
